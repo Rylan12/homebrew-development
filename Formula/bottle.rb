31cb4dd003465115e0d5eb3e591ce26fa4233e43
Rylan Polster <rslpolster@gmail.com>
2020-10-15 14:02:45 -0400

    bottle 0.7 (new formula)

---
+++ Formula/bottle.rb
@@ -0,0 +1,23 @@
+class Bottle < Formula
+  desc "Aescrypt: Program for encryption/decryption"
+  homepage "https://aescrypt.sourceforge.io/"
+  url "https://aescrypt.sourceforge.io/aescrypt-0.7.tar.gz"
+  sha256 "7b17656cbbd76700d313a1c36824a197dfb776cadcbf3a748da5ee3d0791b92d"
+
+  conflicts_with "aescrypt", because: "both install `aescrypt` and `aesget` binaries"
+
+  def install
+    system "./configure"
+    system "make"
+    bin.install "aescrypt", "aesget"
+  end
+
+  test do
+    (testpath/"key").write "kk=12345678901234567890123456789abc0"
+    original_text = "hello"
+    cipher_text = pipe_output("#{bin}/aescrypt -k #{testpath}/key -s 128", original_text)
+    deciphered_text = pipe_output("#{bin}/aesget -k #{testpath}/key -s 128", cipher_text)
+    assert_not_equal original_text, cipher_text
+    assert_equal original_text, deciphered_text
+  end
+end
